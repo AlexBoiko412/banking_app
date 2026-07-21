5aaaf3d278d942a67f0ea795b04b456d95e0de59
AlexBoiko <bojkosasa78@gmail.com>
2026-07-21 18:37:20 +0200

    fix

--- src/main/java/com/banking_app/banking_app/exceptions/UserNotFoundException.java
+++ src/main/java/com/banking_app/banking_app/exceptions/UserNotFoundException.java
@@ -4,4 +4,7 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
         super("User with id: " + id + " not found.");
     }
+    public UserNotFoundException(String username) {
+        super("User: " + username + " not found.");
+    }
 }
