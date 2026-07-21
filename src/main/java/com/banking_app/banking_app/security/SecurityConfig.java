77aa2b7cc1228bc2778154b67f6a8834dc25b101
AlexBoiko <bojkosasa78@gmail.com>
2026-07-21 18:38:32 +0200

    added roles req to /users

--- src/main/java/com/banking_app/banking_app/security/SecurityConfig.java
+++ src/main/java/com/banking_app/banking_app/security/SecurityConfig.java
@@ -8,19 +8,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
                 .authorizeHttpRequests(auth ->
-                    auth.requestMatchers("/auth/**", "/users").permitAll()
+                    auth.requestMatchers("/auth/**").permitAll()
+                            .requestMatchers("/users").hasRole("ADMIN")
                             .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
