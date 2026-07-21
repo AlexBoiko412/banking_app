f400c94a58776d05a6bef0469d387518cac03526
AlexBoiko <bojkosasa78@gmail.com>
2026-07-21 18:39:05 +0200

    added spring userdetailsservice implementation

---
+++ src/main/java/com/banking_app/banking_app/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.banking_app.banking_app.security;
+
+import com.banking_app.banking_app.entities.User;
+import com.banking_app.banking_app.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPasswordHash())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
