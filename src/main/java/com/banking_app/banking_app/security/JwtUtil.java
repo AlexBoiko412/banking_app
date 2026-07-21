3dc23a7536c9e19e7f2f56405dc57b12d2951006
AlexBoiko <bojkosasa78@gmail.com>
2026-07-21 11:26:35 +0200

    added jwt util

---
+++ src/main/java/com/banking_app/banking_app/security/JwtUtil.java
@@ -0,0 +1,41 @@
+package com.banking_app.banking_app.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+    private final long expiration;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long expiration) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.expiration = expiration;
+    }
+
+    public String generateToken(String email, String login) {
+        return Jwts.builder()
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .subject(email)
+                .issuedAt(new Date())
+                .claim("login", login)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Claims validateToken(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
