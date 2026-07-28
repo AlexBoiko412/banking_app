b64dfe342dd53c0ebed59d1557989ab70567e09e
AlexBoiko <bojkosasa78@gmail.com>
2026-07-28 14:42:50 +0200

    added auth controller

---
+++ src/main/java/com/banking_app/banking_app/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package com.banking_app.banking_app.controllers;
+
+import com.banking_app.banking_app.dtos.CreateUserRequest;
+import com.banking_app.banking_app.dtos.LoginRequest;
+import com.banking_app.banking_app.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(
+                authService.login(request.getEmail(), request.getPassword())
+        );
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody CreateUserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                authService.register(request)
+        );
+    }
+}
